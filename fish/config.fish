4e88428dd41c42c6e186caa415f285df703aaf47
Evgenii Kotelnikov <evgeny.kotelnikov@gmail.com>
2021-03-06 16:10:31 +0100

    Add git.fish

--- fish/config.fish
+++ fish/config.fish
@@ -6,21 +6,11 @@ export LC_ALL="en_US.UTF-8"
 
 set fish_greeting
 
-abbr --add g     git
-abbr --add gita  'git add'
-abbr --add gitl  'git log'
-abbr --add gitd  'git diff'
-abbr --add gitds 'git diff --staged'
-abbr --add gits  'git status'
-abbr --add gitc  'git commit'
-abbr --add gitcp 'git cherry-pick'
-abbr --add gitb  'git blame'
-abbr --add gitco 'git checkout'
-abbr --add gitg  'git grep'
-
 abbr --add untar 'tar -xzf'
 
 abbr --add vi vim
 
 alias l='exa -ahl --group-directories-first'
 alias lt='l -T'
+
+source (dirname (status --current-filename))/git.fish
---
+++ fish/git.fish
@@ -0,0 +1,17 @@
+# Abbreviations
+abbr --add g     git
+abbr --add gr    'git rebase'
+abbr --add gita  'git add'
+abbr --add gitl  'git log'
+abbr --add gitd  'git diff'
+abbr --add gitds 'git diff --staged'
+abbr --add gits  'git status'
+abbr --add gitc  'git commit'
+abbr --add gitcp 'git cherry-pick'
+abbr --add gitb  'git blame'
+abbr --add gitco 'git checkout'
+abbr --add gitg  'git grep'
+abbr --add gitgw 'git grep -w'
+
+# Complete git-rebase with recent commits
+complete -k -f -c git -n '__fish_git_using_command rebase' -a '(__fish_git_recent_commits)'
