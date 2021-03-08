56964d518f75e3b88216d5e452a6d38f79a7525a
Evgenii Kotelnikov <evgeny.kotelnikov@gmail.com>
2021-03-08 10:51:07 +0100

    Better way of reading git branch

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,4 +1,4 @@
 function fish_right_prompt
-    set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
+    set -l git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
     printf '%s%s%s' (set_color brblack) "$git_branch" (set_color normal)
 end
