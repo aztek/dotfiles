e86d28059f0852dab9441800bc8e175412e562c8
Evgenii Kotelnikov <evgenii.kotelnikov@zenseact.com>
2021-03-31 12:43:17 +0200

    Add more git abbrs

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -2,7 +2,9 @@
 abbr --add g     git
 abbr --add gr    'git rebase'
 abbr --add gri   'git rebase --interactive'
+abbr --add grc   'git rebase --continue'
 abbr --add gita  'git add'
+abbr --add gitau 'git add --update'
 abbr --add gitl  'git log'
 abbr --add gitd  'git diff'
 abbr --add gitds 'git diff --staged'
@@ -12,7 +14,7 @@ abbr --add gitcp 'git cherry-pick'
 abbr --add gitb  'git blame'
 abbr --add gitco 'git checkout'
 abbr --add gitg  'git grep'
-abbr --add gitgw 'git grep -w'
+abbr --add gitgw 'git grep --word-regexp'
 
 # Complete git-rebase with 10 recent commits
 complete -k -f -c git -n '__fish_git_using_command rebase' -a '(__fish_git_recent_commits | head -n10)'
