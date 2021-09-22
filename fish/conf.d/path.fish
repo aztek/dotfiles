4c579db7bf2d2e278170439837c5b777736c36f8
Evgenii Kotelnikov <evgenii.kotelnikov@zenseact.com>
2021-09-22 11:32:45 +0200

    Add OPAM initialization in Fish

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -11,6 +11,11 @@ if test -d ~/Library/Haskell/bin
   set -gx PATH $PATH ~/Library/Haskell/bin
 end
 
+# OPAM
+if test -f ~/.opam/opam-init/init.fish
+  source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+end
+
 # Misc
 if test -d ~/.local/bin
   set -gx PATH ~/.local/bin $PATH
