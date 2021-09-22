d77131405894bb81bd0862491d8d8cecb7bb1cdf
Evgenii Kotelnikov <evgenii.kotelnikov@zenseact.com>
2021-09-22 11:22:50 +0200

    Only show git branch in interactive shells

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,4 +1,6 @@
 function fish_right_prompt
-    set -l git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
-    printf '%s%s%s' (set_color brblack) "$git_branch" (set_color normal)
+    if status is-interactive
+        set -l git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
+        printf '%s%s%s' (set_color brblack) "$git_branch" (set_color normal)
+    end
 end
