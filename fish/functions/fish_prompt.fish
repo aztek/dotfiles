d2b5a057dc64b036b9079793a65fb7a219f81bb0
Evgenii Kotelnikov <evgenii.kotelnikov@zenseact.com>
2022-05-02 15:33:34 +0200

    Display hostname in ssh fish sessions

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,9 @@
 function fish_prompt --description 'Write out the prompt'
-    if test -z $WINDOW
+    if test -z "$WINDOW"
+        if test -n "$SSH_TTY$SSH_CLIENT"
+            printf '%s%s%s' (set_color $fish_color_host_remote) (hostname) (set_color normal)
+            printf ':'
+        end
         printf '%s%s%s$ ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
     else
         printf '%s(%s)%s%s%s$ ' (set_color white) (echo $WINDOW) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
